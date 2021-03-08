edef1b1d1a0477d0e404c8801e24e5347ed8ffb5
Alphena <alphena@gmail.com>
2021-03-07 19:02:11 -0700

    fully populated hour rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,20 +25,34 @@ Global Variable
 */
 
 var today = moment().format('dddd MMMM Do YYYY');
-var currentTime = moment().format('HH mm');
+var currentTime = moment().format('HH');
 
 var activeHours = [
-    {hour: '9'}, 
-    {hour:'10'}, 
-    {hour: '11'},
-    {hour: '12'}, 
-    {hour: '13'}, 
-    {hour:'14'},
-    {hour: '15'}, 
-    {hour:'16'}, 
-    {hour:'17'},
+    // {hour: '00',},
+    // {hour: '01',},
+    // {hour: '02',},
+    // {hour: '03',},
+    // {hour: '04',},
+    // {hour: '05',},
+    // {hour: '06',},
+    // {hour: '07',},
+    // {hour: '08',},
+    // {hour: '09',}, 
+    // {hour: '10',}, 
+    // {hour: '11',},
+    // {hour: '12',}, 
+    // {hour: '13',}, 
+    // {hour: '14',},
+    // {hour: '15',}, 
+    {hour: '16',}, 
+    {hour: '17',},
+    {hour: '18',},
+    {hour: '19',},
+    {hour: '20',},
+    {hour: '21',},
+    {hour: '22',},
+    {hour: '23',},
 ];
-
 
 /*
 Functions
@@ -53,32 +67,58 @@ function revealTime(){
 
 revealTime()
 
-// generate each hour's spaces
-
-
+// generate each hour's spaces on the page
 activeHours.forEach(function (dewIt){ 
+    //creates the rows to populate  
+    //defines the area we want to interact with
     var planDay = $('.container');
+    //creates new element
     var hourRow = $('<div>');
+    //adds the css to the element
     hourRow.addClass("row");
-    // planDay.append(hourRow);
-    
-    var hourList = $('<div>');
-    hourList.addClass(" col-md-1 hour");
-    hourList.text(activeHours.hour);
-    hourRow.append(hourList);
+    //applies it to the page
     planDay.append(hourRow);
+    
+    // adds the individual hours to the space
+    // creates the new element
+    var hourList = $('<div>');
+    //adds the css to the element
+    hourList.addClass("col-1 hour");
+    //pulls the indvidual text from the array, based on the key
+    hourList.text(dewIt.hour);
+    //creates the new element on the page
+    hourRow.append(hourList);
+   
+    // adds the event saved to the rows
+    // creates the new element
+    var hourEvent = $('<textarea>');
+    //dynamically add css based on if the hour is before, ===, or after the currentTime
+    if(dewIt.hour === currentTime){
+        hourEvent.addClass("col-10 description present")
+    }else if(dewIt.hour > currentTime) {
+        hourEvent.addClass("col-10 description future")
+    }else if(dewIt.hour < currentTime){
+        hourEvent.addClass("col-10 description past")
+    };
+    //adds the element to the page
+    hourRow.append(hourEvent);
+    
+    //adds the save buttons to the rows
+    //creates the new element
+    var saveEvent = $('<button>');
+    //adds the css to the element
+    saveEvent.addClass('col-1 saveBtn');
+    //adds element to the page
+    hourRow.append(saveEvent);
 });
-
-// row 
-// hours 
-// entry
-// saveBtn
-
 
 
 /*
 Events
 */
+
+// event listener on saveBtn 
+
 
 /*
 Entry Points
